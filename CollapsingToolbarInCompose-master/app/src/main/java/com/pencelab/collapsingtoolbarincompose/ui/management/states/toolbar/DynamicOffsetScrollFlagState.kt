9803ff04da5d84865742f79b6aa62c70ea605f04
phucntt <vanphuc.earn.money@gmail.com>
2023-02-03 11:29:57 +0700

    update

--- CollapsingToolbarInCompose-master/app/src/main/java/com/pencelab/collapsingtoolbarincompose/ui/management/states/toolbar/DynamicOffsetScrollFlagState.kt
+++
@@ -1,9 +0,0 @@
-package com.pencelab.collapsingtoolbarincompose.ui.management.states.toolbar
-
-abstract class DynamicOffsetScrollFlagState(
-    heightRange: IntRange,
-    scrollValue: Int
-) : ScrollFlagState(heightRange, scrollValue) {
-
-    protected abstract var scrollOffset: Float
-}
